440c6ec8bd875ff198424bcba14c79302f279a62
Anand Inguva <anandinguva98@gmail.com>
2024-03-19 18:24:32 -0400

    change topics and bootstrapServers in GMKConstants

--- src/main/java/org/example/GMKConstants.java
+++ src/main/java/org/example/GMKConstants.java
@@ -1,7 +1,7 @@
 package org.example;
 
 public class GMKConstants {
-    public static String bootStrapServers = "https://localhost:9092";
-    public static String topic = "test-topic-1";
+    public static String bootStrapServers = " bootstrap.anandinguva-test-cluster.us-central-1.managedkafka-staging.dataflow-testing-311516.cloud-staging.goog:9092";
+    public static String topic = "anandinguva-test-topic";
 
 }
