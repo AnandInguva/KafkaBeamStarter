2c465f8d1c94dcc50dc36f64d0d79771f1850998
Anand Inguva <anandinguva98@gmail.com>
2024-04-02 13:25:14 -0400

    Change replicationFactor to 3

--- src/main/java/org/example/GMKConstants.java
+++ src/main/java/org/example/GMKConstants.java
@@ -2,9 +2,9 @@ package org.example;
 
 public class GMKConstants {
     public static String bootStrapServers = "bootstrap.anandinguva-test-cluster.us-central1.managedkafka-staging.dataflow-testing-311516.cloud-staging.goog:9092";
-    public static String topic = "avro-records";
+    public static String topic = "avro-records-1";
 
-    public static int partitions = 2;
+    public static int partitions = 3;
 
 //    public static String bootStrapServers = "localhost:9092";
 
