df2bcb12926c99b866197bd6e800bf401ad4699a
Anand Inguva <anandinguva98@gmail.com>
2024-04-17 17:55:31 -0400

    change topic name

--- src/main/java/org/example/GMKConstants.java
+++ src/main/java/org/example/GMKConstants.java
@@ -3,7 +3,7 @@ package org.example;
 
 public class GMKConstants {
     public static String bootStrapServers = "bootstrap.anandinguva-test-cluster.us-central1.managedkafka-staging.dataflow-testing-311516.cloud-staging.goog:9092";
-    public static String topic = "json-string";
+    public static String topic = "avro-records-3";
 
     public static int partitions = 3;
     public static Integer replicationFactor = 1;
